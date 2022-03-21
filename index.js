90ae64c84907a73bdababffadb5cddbc74696abb
johannafryxell <johanna.fryxell@gmail.com>
2022-03-21 15:57:09 +0100

    Style setup

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("./mongoose");
 const express = require("express");
 const exphbs = require("express-handlebars");
 const jwt = require("jsonwebtoken");
+const loginRouter = require("./routes/login-routes");
 const cookieParser = require("cookie-parser");
 
 const app = express();
@@ -21,6 +22,11 @@ app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static("public"));
+
+/////////////
+// ROUTES //
+///////////
+app.use("/login", loginRouter);
 
 app.get("/", async (req, res) => {
   res.render("home");
--- routes/login-routes.js
+++ routes/login-routes.js
@@ -2,4 +2,8 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 
+router.get("/", (req, res) => {
+  res.render("login");
+});
+
 module.exports = router;
