eae079af2448953c955e149e8700e8b2ee1f9c2c
julialotta <julialottatinglof@gmail.com>
2022-03-21 09:47:44 +0100

    Project setup

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+require("./mongoose");
+
+const express = require("express");
+const exphbs = require("express-handlebars");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.engine(
+  "hbs",
+  exphbs.engine({
+    defaultLayout: "main",
+    extname: ".hbs",
+    helpers: {},
+  })
+);
+
+app.set("view engine", "hbs");
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  res.render("home");
+});
+
+app.listen(8000, () => {
+  console.log("http://localhost:8000");
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+Städa fint AB
+
+Start SASS:
+npm run sass
+
+To run this application you need these things in the .env file.
+CONNECTION_STRING=""
+JWTSECRET=""
