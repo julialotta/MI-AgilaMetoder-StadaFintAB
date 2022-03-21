b59e47697594dba4e0abbb26b0c5b3b2ee987c01
julialotta <julialottatinglof@gmail.com>
2022-03-21 11:48:55 +0100

    UsersModel added

---
+++ models/UsersModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  name: { type: String, required: true },
+  adress: { type: String, required: true },
+  phone: { type: Number, required: true },
+  hashedPassword: { type: String, required: true },
+});
+
+const UsersModel = mongoose.model("Users", userSchema);
+
+module.exports = UsersModel;
