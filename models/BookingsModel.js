c60139e5a027348af82debb255aa7d420adb77b8
Malin Nilsson <malinnilsson87@gmail.com>
2022-03-21 12:47:30 +0100

    Added form to hbs and the basics for routes

--- models/BookingsModel.js
+++ models/BookingsModel.js
@@ -1,10 +1,10 @@
 const { Schema, model } = require("mongoose");
 
 const bookingSchema = new Schema({
-    date: { type: String, required: true },
-    time: { type: Number, default: Date.now },
-    cleanerId: { type: Schema.Types.ObjectId, ref: "Cleaners", required: true },
-    userId: { type: Schema.Types.ObjectId, ref: "Users", requires: true }
+    date: { type: String, required: true, default: Date.now },
+    time: { type: String, required: true },
+    cleaner: { type: Schema.Types.ObjectId, ref: "Cleaners", required: false },
+    user: { type: Schema.Types.ObjectId, ref: "Users", required: false }
   });
   
   const BookingsModel = model("Bookings", bookingSchema);
--- routes/bookings-routes.js
+++ routes/bookings-routes.js
@@ -5,11 +5,31 @@ const router = express.Router();
 
 // GET - BOOK A CLEANING //
 router.get("/", (req, res) => {
-      res.render("bookings/book-cleaning");
-  });
+  res.render("bookings/book-cleaning");
+});
 
 
 // POST – BOOK A CLEANING //
+router.post("/", async (req, res) => {
+  const { date, time } = req.body
+
+  const newBooking = new BookingsModel({
+    date: date,
+    time: time,
+    // user: {
+    //   _id: user._id,
+    // },
+    // cleaner: {
+    //   _id: cleaner._id
+    // }
+  });
+
+  await newBooking.save();
+
+  console.log(newBooking)
+
+  res.render("bookings/book-cleaning");
+});
 
 
 module.exports = router;
