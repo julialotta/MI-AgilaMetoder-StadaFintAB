b27d209a5919575f72092895ab6cdedfaa759f66
johannafryxell <johanna.fryxell@gmail.com>
2022-03-21 21:39:40 +0100

    compare password

--- routes/login-routes.js
+++ routes/login-routes.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const UsersModel = require("../models/UsersModel.js");
+const { comparePassword } = require(".././utils");
 
 router.get("/", (req, res) => {
   res.render("login");
+});
+
+router.post("/", async (req, res) => {
+  const { email, password } = req.body;
+    
+  UsersModel.findOne({ email }, (err, user) => {
+      if (user && comparePassword(password, user.hashedPassword)){
+        const userData = { userId: user._id, email };
+        const accessToken = jwt.sign(userData, process.env.JWTSECRET);
+
+        res.cookie("token", accessToken);
+        res.redirect("/");
+      } else {
+          res.render("login", {loginFailed: true})
+      }
+  });
 });
 
 module.exports = router;
