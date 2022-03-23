8baaca9f32703168362fb770ca5c25f227add271
Malin Nilsson <malinnilsson87@gmail.com>
2022-03-23 11:19:22 +0100

    Testing availability function

--- utils.js
+++ utils.js
@@ -28,13 +28,44 @@ const comparePassword = (password, hash) => {
   return correct;
 };
 
-const getCleaner = async () => {
-  const cleaners = await CleanersModel.find().lean();
+const getCleaner = async (date, time) => {
+  // Hämta alla städare från CleanersModel
+  let cleaners = await CleanersModel.find().lean();
+  // Hämta alla bokningar från BookingsModel
+  let bookings = await BookingsModel.find().lean();
 
-  // Slumpa fram en städare
+  // Slumpa fram en städare från CleanersModel
   const randomCleaner = cleaners[Math.floor(Math.random() * cleaners.length)];
+  
+  // Lägg städarens id i en ny variabel
   let cleanerId = randomCleaner._id;
-  console.log(cleanerId)
+  
+  // Testkod för att kolla tillgänglighet - fungerar ej just nu :( 
+
+  // Skapa ny lista för städare
+  // let newCleanerslist = []
+
+  // Hämta id från alla städare
+  // cleaners.forEach(cleaner => {
+  //   let id = cleaner._id;
+  //   // Pusha dem till nya listan
+  //   newCleanerslist.push(id)
+  // });
+
+  // Shuffla listan med städare
+  // newCleanerslist = newCleanerslist.sort(() => Math.random() - 0.5)
+
+  // Kolla om städaren i bokningen är uppbokad på samma datum & tid
+  // Just nu kollar den cleanerId från rad 41, inte id:et från shufflade listan
+
+  // bookings.forEach(booking => {
+  //   if (booking.cleaner === cleanerId && booking.date === date && booking.time === time) {
+  //     console.log("Cleaner is busy")
+  //   } else {
+  //     return cleanerId
+  //   }
+  // });
+
   return cleanerId
 }
 
