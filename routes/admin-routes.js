4634bc17219fd2e54a4d9c836745a67b0c26ee0b
Malin Nilsson <malinnilsson87@gmail.com>
2022-03-24 20:16:43 +0100

    Post requests for admin/employee page

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -4,6 +4,7 @@ const jsonwebtoken = require("jsonwebtoken");
 const jwt = require("jsonwebtoken");
 const UsersModel = require("../models/UsersModel.js");
 const CleanersModel = require("../models/CleanersModel.js")
+const BookingsModel = require("../models/BookingsModel.js")
 
 router.get("/customers", (req, res) => {
   res.render("admin/admin-clients");
@@ -16,5 +17,31 @@ router.get("/employees", async (req, res) => {
     employees
   });
 });
+
+router.post("/employees", async (req, res) => {
+  const employees = await CleanersModel.find().lean()
+
+  for (let i = 0; i < employees.length; i++) {
+    if (req.body.employee == employees[i]._id) {
+      let selectedEmployee = employees[i]._id;
+
+      const bookings = await BookingsModel.find({
+        cleaner: selectedEmployee
+      }).lean()
+
+
+      res.render("admin/admin-employees", {
+        employees,
+        selectedEmployee,
+        bookings
+      });
+    }
+  }
+})
+
+router.post("/employee/delete", async (req, res) => {
+  await BookingsModel.findById(req.params.id).deleteOne();
+  res.redirect("/employees");
+})
 
 module.exports = router;
