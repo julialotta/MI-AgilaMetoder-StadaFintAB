263850e385f477bc75d7d7c24497d85c92f985a3
Malin Nilsson <malinnilsson87@gmail.com>
2022-03-24 14:56:47 +0100

    Show employees in dropdown

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -2,14 +2,19 @@ const express = require("express");
 router = express.Router();
 const jsonwebtoken = require("jsonwebtoken");
 const jwt = require("jsonwebtoken");
-const UsersModel = require("../models/UsersModel");
+const UsersModel = require("../models/UsersModel.js");
+const CleanersModel = require("../models/CleanersModel.js")
 
 router.get("/customers", (req, res) => {
   res.render("admin/admin-clients");
 });
 
-router.get("/employees", (req, res) => {
-  res.render("admin/admin-employees");
+router.get("/employees", async (req, res) => {
+  const employees = await CleanersModel.find().lean()
+  console.log(employees)
+  res.render("admin/admin-employees", {
+    employees
+  });
 });
 
 module.exports = router;
