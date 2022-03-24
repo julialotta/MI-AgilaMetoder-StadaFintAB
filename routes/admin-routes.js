92c932cd7d583388f79d1a8a62736e9a2a645d28
Sofia Borg <fia.borg@hotmail.com>
2022-03-24 12:00:14 +0100

    style done ish

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -4,6 +4,8 @@ const jsonwebtoken = require("jsonwebtoken");
 const jwt = require("jsonwebtoken");
 const UsersModel = require("../models/UsersModel");
 
-
+router.get("/customers", (req, res) => {
+  res.render("admin/admin-clients");
+});
 
 module.exports = router;
