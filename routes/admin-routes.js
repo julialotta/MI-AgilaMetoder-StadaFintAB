609a52dffb98918e0eeb71a2e6b415ebfed0add3
Sofia Borg <fia.borg@hotmail.com>
2022-03-24 16:27:05 +0100

    rendering bookings work

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -3,9 +3,37 @@ router = express.Router();
 const jsonwebtoken = require("jsonwebtoken");
 const jwt = require("jsonwebtoken");
 const UsersModel = require("../models/UsersModel");
+const BookingsModel = require("../models/BookingsModel");
 
-router.get("/customers", (req, res) => {
-  res.render("admin/admin-clients");
+router.get("/customers", async (req, res) => {
+  //hämta alla users/customers och rendera i selecion list
+  const allCustomers = await UsersModel.find({ admin: { $ne: true } }).lean();
+
+  res.render("admin/admin-clients", { allCustomers });
+});
+
+router.post("/customers", async (req, res) => {
+  //en post sker när man klickar på en user i listan. Då renderas rätt info om usern.
+  const allCustomers = await UsersModel.find({ admin: { $ne: true } }).lean();
+
+  for (let i = 0; i < allCustomers.length; i++) {
+    if (req.body.selectedList == allCustomers[i]._id) {
+      const selectedUser = allCustomers[i]._id;
+
+      const bookingInfo = await BookingsModel.find({
+        user: selectedUser,
+      })
+        .populate("cleaner")
+        .lean();
+
+      console.log(selectedUser);
+      res.render("admin/admin-clients", {
+        allCustomers,
+        bookingInfo,
+        selectedUser,
+      });
+    }
+  }
 });
 
 module.exports = router;
