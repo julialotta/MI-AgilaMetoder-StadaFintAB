9cc8f7f235ba783a9e24e16f78e096fa4573f3a0
johannafryxell <johanna.fryxell@gmail.com>
2022-03-28 09:51:47 +0200

    Final bugfix

--- routes/login-routes.js
+++ routes/login-routes.js
@@ -6,13 +6,16 @@ const CleanersModel = require("../models/CleanersModel.js");
 const { comparePassword } = require(".././utils");
 
 router.get("/", (req, res) => {
-  res.render("login");
+  res.render("login", {customer: true});
 });
 
-router.post("/", async (req, res, next) => {
+router.get("/employee", (req,res) => {
+  res.render("login", {customer: false});
+})
+
+router.post("/", async (req, res) => {
   const { email, password } = req.body;
   
-  //Söker bland kunder
   UsersModel.findOne({ email }, (err, user) => {
       if (user && comparePassword(password, user.hashedPassword)){
         const userData = { userId: user._id, email };
@@ -20,21 +23,27 @@ router.post("/", async (req, res, next) => {
         res.cookie("token", accessToken);
         res.redirect("/customer/mypage");
       } else {
-        res.render("login", {loginFailed: true})
+        res.render("login", {loginFailed: true, customer: true})
       }
   });
 
-  //Söker bland städare
+  
+});
+router.post("/cleaner", async (req,res) => {
+  const { email, password } = req.body;
+ 
   CleanersModel.findOne({email}, (err, cleaner) => {
     if (cleaner && comparePassword(password, cleaner.hashedPassword)){
       const cleanerData = { cleanerId: cleaner._id, email };
       const accessToken = jwt.sign(cleanerData, process.env.JWTSECRET);
-
+  
       res.cookie("token", accessToken);
       res.redirect("/cleaner/mypage");
-    } 
+    } else {
+      res.render("login", {loginFailed: true, customer: false})
+    }
   }) 
 
-});
+})
 
 module.exports = router;
